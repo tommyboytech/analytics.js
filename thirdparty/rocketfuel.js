43ec6fe96673cf1e86efeb7db99ba8c3b83fe4d5
Trey <trey.walker@gmail.com>
2018-10-31 16:09:26 -0700

    ENG-1686 more generic

--- thirdparty/rocketfuel.js
+++ thirdparty/rocketfuel.js
@@ -14,7 +14,7 @@ var integration = require('@segment/analytics.js-integration');
 var RocketFuel = module.exports = integration('Rocket Fuel')
   .option('accountId', '')
   .option('universalActionId', '')
-  .tag('stargate step', '<img src="//{{ actionId }}p.rfihub.com/ca.gif?rb={{ accountId }}&ca={{ actionId }}&ra={{ cacheBuster }}&_o={{ accountId }}&_t={{ actionId }}"/>')
+  .tag('track', '<img src="//{{ actionId }}p.rfihub.com/ca.gif?rb={{ accountId }}&ca={{ actionId }}&ra={{ cacheBuster }}&_o={{ accountId }}&_t={{ actionId }}"/>')
   .tag('universal', '<img src="//{{ universalActionId }}p.rfihub.com/ca.gif?rb={{ accountId }}&ca={{ universalActionId }}&_o=37037&_t=20809061&ra={{ cacheBuster }}"/>')
   .mapping('events');
 
@@ -43,20 +43,16 @@ RocketFuel.prototype.page = function() {
  */
 
 RocketFuel.prototype.track = function(track) {
-  var eventName = track && track.obj && track.obj.event;
+  var actionId = track && track.obj && track.obj.properties && track.obj.properties.actionId;
   var events = this.events(track.event());
   var self = this;
-  var eventActionIdMap = {
-    'Load Homepage': '20809060',
-    'Click Join Now': '20809169',
-    'Click Stargate Next': '20809170',
-    'Click SoundCloud Connect': '20809171',
-    'Click YouTube Connect': '20809172',
-    'Click Stargate Skip': '20809062',
+
+  if (!actionId) {
+    return;
   }
 
-  return self.load('stargate step', {
-    actionId: eventActionIdMap[eventName] || '',
+  return self.load('track', {
+    actionId: actionId,
     cacheBuster: self.cacheBuster()
   });
 };
